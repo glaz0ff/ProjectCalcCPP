150aa93950142f497d6c68a986294b3fb9b456c1
AlexndrV59 <kent1234810@gmail.com>
2020-11-08 20:31:49 +0500

    Calculator with 5 func()

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,b;
+    char c;
+    cin >> a >> c >> b;
+    switch(c)
+    {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '/': cout << a / b; break;
+        case '%': cout << int(a) % int(b); break;
+    }
+    return 0;
+}
